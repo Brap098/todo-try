2cc544c188e9bdbb19609348cc99b2602e9cf456
Brap098 <Brapagon.Work@gmail.com>
2022-07-03 11:55:05 -0400

    add: bootstrap, img, etc

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    let places = []
+    let places = [{
+        name: 'J-Tops bbq shop',
+        city: 'clayton',
+        state: 'NC',
+        cuisines: 'BBQ, Ribs, Chicken, ETC',
+        pic: 'public\images\JTop_s-logo-greyscale-white.png'
+      }, {
+          name: 'fivestarraleigh',
+          city: 'Raleigh',
+          state: 'NC',
+          cuisines: 'chinese, drinks',
+          pic: 'public\images\81806fivestar_logo.png'
+      }]
+      
     res.render('places/index', {places})
 })
 
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>OOPS! sorry page was lost!</p>
+                <div>
+      <img src="https://cdn-acpnj.nitrocdn.com/SDkrhncnWeetGsYGlzwaPnbfptfOeIKk/assets/static/optimized/rev-00d8738/wp-content/uploads/2020/01/Free-Hilarious-Anime-Memes-7.jpg" alt="ME most classes" />
+      <div>
+        Photo by <a href="creator">She hates memes</a> on <a href="https://memesbams.com/anime-memes/">memesbams</a>
+      </div>
+    </div>
             </main>
         </Def>
     )
