bfa2f7653101959bb9066fb5529d395106bb7d19
Brap098 <Brapagon.Work@gmail.com>
2022-07-05 17:58:30 -0400

    new places added

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,28 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'J-Tops bbq shop',
-        city: 'clayton',
-        state: 'NC',
-        cuisines: 'BBQ, Ribs, Chicken, ETC',
-        pic: '/images/JTop_s-logo-greyscale-white.png'
-      }, {
-          name: 'fivestarraleigh',
-          city: 'Raleigh',
-          state: 'NC',
-          cuisines: 'chinese, drinks',
-          pic: '/images/81806fivestar_logo.png'
-      }]
-      
-    res.render('places/index', {places})
+    console.log(req.body)
+    res.render('places/index', { places })
 })
 
+
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    //console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
 
 
 router.get('/new', (req, res) => {
